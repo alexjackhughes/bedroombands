f8f459a280e6eedbca244c02bf1eef78ec27ca50
alexjackhughes <hughes@huckletree.com>
2018-06-24 10:31:57 +0100

    fixing routing problem

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ if(process.env.NODE_ENV === 'production') {
 
   // Express will serve up correct client stuff
   const path = require('path');
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
