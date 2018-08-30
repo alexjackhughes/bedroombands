0fc1039f7fb4a2f177230eb02508763444937165
alexjackhughes <hughes@huckletree.com>
2018-08-30 18:05:54 +0100

    Generic tracks are mobile-optimized

--- client/src/components/GenericTracks/index.js
+++ client/src/components/GenericTracks/index.js
@@ -6,7 +6,6 @@ import TrackList from "../TrackList";
 import Track from "../Track";
 
 class GenericTracks extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -16,52 +15,49 @@ class GenericTracks extends Component {
 
   // need to add tracks
   componentDidMount() {
-    if(this.props.match.params.type == 'rating') {
-
+    if (this.props.match.params.type == "rating") {
       let rating = this.props.match.params.rating;
-      this.setState({title: 'Rated ' + rating + '*'});
+      this.setState({ title: "Rated " + rating + "*" });
 
       axios.get(`/api/tracks/rating/${rating}`).then(tracks => {
         this.setState({ tracks: tracks.data });
-        console.log('data from api', tracks);
+        console.log("data from api", tracks);
       });
-
-
     } else {
-      let title = this.props.match.params.type.replace(/\b\w/g, l => l.toUpperCase());
+      let title = this.props.match.params.type.replace(/\b\w/g, l =>
+        l.toUpperCase()
+      );
       let type = this.props.match.params.type;
-      this.setState({title: title});
+      this.setState({ title: title });
 
       axios.get(`/api/tracks/type/${type}`).then(tracks => {
         this.setState({ tracks: tracks.data });
-        console.log('data from api', tracks);
+        console.log("data from api", tracks);
       });
-
     }
   }
 
   render() {
-    if(this.state && this.state.title && this.state.tracks.length !== 0) {
-          return(
-
+    if (this.state && this.state.title && this.state.tracks.length !== 0) {
+      return (
+        <div className="row">
+          <div className="centre col s8 offset-s2 track-mobile">
+            <h1 className="profile-title">{this.state.title} Tracks</h1>
             <div className="row">
-              <div className="centre col s8 offset-s2">
-                <h1 className="profile-title">{this.state.title} Tracks</h1>
-                <div className="row">
-                    <TrackList tracks={this.state.tracks} />
-                </div>
-              </div>
+              <TrackList tracks={this.state.tracks} />
             </div>
-          );
+          </div>
+        </div>
+      );
     }
-    return(
+    return (
       <div>
         <div className="row">
           <div className="centre col s8 offset-s2">
             <h1 className="profile-title">Tracks</h1>
             <p>Looks like you don't have any tracks here yet :(</p>
             <div className="row">
-                <TrackList tracks={this.state.tracks} />
+              <TrackList tracks={this.state.tracks} />
             </div>
           </div>
         </div>
--- client/src/components/Track/index.js
+++ client/src/components/Track/index.js
@@ -4,69 +4,72 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
-import NotFound from '../NotFound';
+import NotFound from "../NotFound";
 
 class Track extends Component {
-
   constructor() {
     super();
     this.state = {
       trackRated: false
-    }
+    };
   }
 
   componentDidMount() {
-
     let users = [];
 
     this.props.track.artists.forEach(artist => {
-      axios.get(`/api/user/${artist}`).then(user => {
-        users.push(user.data);
-      }).then((data) => {
-        this.setState({ users: users });
-        console.log('users', users);
-      });
+      axios
+        .get(`/api/user/${artist}`)
+        .then(user => {
+          users.push(user.data);
+        })
+        .then(data => {
+          this.setState({ users: users });
+          console.log("users", users);
+        });
     });
   }
 
   // Rating track - need to make api call
   rateTrack(e, rating) {
     let id = this.props.track._id;
-    console.log('rating', rating);
+    console.log("rating", rating);
 
-    axios.put("/api/rate-track/"+ id +"/rating/" + rating)
+    axios
+      .put("/api/rate-track/" + id + "/rating/" + rating)
       // handle success
-      .then((response) => {
+      .then(response => {
         console.log(response);
       })
       // handle error
-      .catch((error) => {
-        console.log('Error', error);
+      .catch(error => {
+        console.log("Error", error);
       })
       // always executed
       .then(() => {
-        this.setState({trackRated: true});
-        console.log('track liked');
+        this.setState({ trackRated: true });
+        console.log("track liked");
       });
   }
 
   editTrack() {
-    console.log('track Edited');
+    console.log("track Edited");
   }
 
   deleteTrack() {
     let id = this.props.track._id;
 
-    console.log('working delete');
+    console.log("working delete");
 
-    axios.delete("/api/track/" + id)
+    axios
+      .delete("/api/track/" + id)
       // handle success
-      .then((response) => {
-        console.log('Track deleted');
+      .then(response => {
+        console.log("Track deleted");
       })
       // handle error
-      .catch((error) => {
-        console.log('Error', error);
+      .catch(error => {
+        console.log("Error", error);
       });
   }
 
@@ -74,28 +77,32 @@ class Track extends Component {
   likeTrack() {
     let id = this.props.track._id;
 
-    axios.put('/api/liked-tracks/' + id)
+    axios
+      .put("/api/liked-tracks/" + id)
       // handle success
-      .then((response) => {
+      .then(response => {
         console.log(response);
       })
       // handle error
-      .catch((error) => {
-        console.log('Error', error);
+      .catch(error => {
+        console.log("Error", error);
       })
       // always executed
       .then(() => {
-        console.log('track liked');
+        console.log("track liked");
         window.location.reload();
       });
   }
 
   renderArtistList(artists) {
-
     let links = [];
 
     artists.map(artist => {
-      links.push(<Link key={artist.id} to={"/artist/" + artist._id} key={artist.id}>{artist.username}</Link>);
+      links.push(
+        <Link key={artist.id} to={"/artist/" + artist._id} key={artist.id}>
+          {artist.username}
+        </Link>
+      );
     });
 
     // Add spacing + commas
@@ -103,13 +110,12 @@ class Track extends Component {
     let counter = 0;
     let artistList = [];
 
-    while(i <= links.length) {
-
+    while (i <= links.length) {
       artistList[i] = links[counter];
 
       // if last artist, don't add comma + spacing - else do
-      if(i !== links.length) {
-        artistList[i+1] = ', ';
+      if (i !== links.length) {
+        artistList[i + 1] = ", ";
         i++;
       }
       counter++;
@@ -120,24 +126,33 @@ class Track extends Component {
   }
 
   renderTags(tags) {
-    return(
-      tags.map((tag) => {
-        return <span key={tag} className="tags">{tag}</span>;
-      })
-    );
+    return tags.map(tag => {
+      return (
+        <span key={tag} className="tags">
+          {tag}
+        </span>
+      );
+    });
   }
 
   renderLikeButton() {
-
     let id = this.props.track._id;
-    console.log('true', this.props.auth.likedTracks.indexOf(id) > -1);
-    if(this.props.auth.likedTracks.indexOf(id) > -1) {
-      return(
-        <a href="#" onClick={() => this.likeTrack()} className="fas fa-heart like-button"></a>
+    console.log("true", this.props.auth.likedTracks.indexOf(id) > -1);
+    if (this.props.auth.likedTracks.indexOf(id) > -1) {
+      return (
+        <a
+          href="#"
+          onClick={() => this.likeTrack()}
+          className="fas fa-heart like-button"
+        />
       );
     } else {
-      return(
-        <a href="#" onClick={() => this.likeTrack()} className="far fa-heart like-button"></a>
+      return (
+        <a
+          href="#"
+          onClick={() => this.likeTrack()}
+          className="far fa-heart like-button"
+        />
       );
     }
   }
@@ -146,11 +161,27 @@ class Track extends Component {
     let stars = [];
 
     for (let index = 0; index < rating; index++) {
-      stars.push(<i key={index} onClick={(e) => {this.rateTrack(e, index+1)}} className="fas fa-star btn-far"></i>);
+      stars.push(
+        <i
+          key={index}
+          onClick={e => {
+            this.rateTrack(e, index + 1);
+          }}
+          className="fas fa-star btn-far"
+        />
+      );
     }
 
     for (let index = rating; index < 5; index++) {
-      stars.push(<i key={index} onClick={(e) => {this.rateTrack(e, index+1)}} className="far fa-star btn-far"></i>);
+      stars.push(
+        <i
+          key={index}
+          onClick={e => {
+            this.rateTrack(e, index + 1);
+          }}
+          className="far fa-star btn-far"
+        />
+      );
     }
 
     return stars;
@@ -158,19 +189,21 @@ class Track extends Component {
 
   // loop through artists and see if id matches logged in user
   renderEditingTools() {
-
     let x = false;
-    this.props.track.artists.map((artist) => {
-      if(String(this.props.auth._id) === String(artist)) {
+    this.props.track.artists.map(artist => {
+      if (String(this.props.auth._id) === String(artist)) {
         x = true;
       }
     });
 
-    if(x) {
-      return(
+    if (x) {
+      return (
         <div className="row">
           {/* <div onClick={() => this.editTrack()} className="far fa-edit track-tools edit-icon" /> */}
-          <div onClick={() => this.deleteTrack()} className="far fa-trash-alt track-tools delete-icon" />
+          <div
+            onClick={() => this.deleteTrack()}
+            className="far fa-trash-alt track-tools delete-icon"
+          />
         </div>
       );
     }
@@ -178,64 +211,78 @@ class Track extends Component {
   }
 
   render() {
-
-    switch (this.state && this.state.track && this.state.users !== undefined
-            && this.state.users.length !== 0 && this.props.auth && this.props.track) {
-
+    switch (
+      this.state &&
+        this.state.track &&
+        this.state.users !== undefined &&
+        this.state.users.length !== 0 &&
+        this.props.auth &&
+        this.props.track
+    ) {
       case null:
         return <span />;
 
       default:
-        console.log('preeprs', track);
+        console.log("preeprs", track);
         let artists = this.state.users;
         let track = this.props.track;
-        let trackUrl = `https://w.soundcloud.com/player/?url=${track.soundCloudUrl}&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`;
+        let trackUrl = `https://w.soundcloud.com/player/?url=${
+          track.soundCloudUrl
+        }&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`;
         return (
-            <div className="row" style={{'textAlign': 'center'}}>
-              <div className="col s10 offset-s1">
-
-                <div className="row">
-                  <div className="track">
-
-                    <div className="track-head">
-                      <div className="row">
-                        <div className="col s8">
-                          <h1 className="track-title">{track.title}</h1>
-                          <div className="track-authors"><span>By</span> {
-                            artists !== undefined ?
-                              this.renderArtistList(artists) :
-                              <span>Loading Artists...</span>
-                          }</div>
-                        </div>
-                        <div className="col s4 like-section">
-                          {this.renderLikeButton()}
+          <div className="row" style={{ textAlign: "center" }}>
+            <div className="col s10 offset-s1 track-mobile">
+              <div className="row">
+                <div className="track">
+                  <div className="track-head">
+                    <div className="row">
+                      <div className="col s8">
+                        <h1 className="track-title">{track.title}</h1>
+                        <div className="track-authors">
+                          <span>By</span>{" "}
+                          {artists !== undefined ? (
+                            this.renderArtistList(artists)
+                          ) : (
+                            <span>Loading Artists...</span>
+                          )}
                         </div>
                       </div>
-                    </div>
-
-                    <iframe width="100%" height="300" scrolling="no" style={{'border':'0'}} allow="autoplay" src={trackUrl}></iframe>
-
-                    <div className="track-bottom">
-                      <div className="row profile-entry track-authors">
-                        <Link to={"/track/" + track._id}>Learn more</Link>
-                      </div>
-                      <div className="row rating-section">
-                        {this.renderRating(track.currentRating)}
+                      <div className="col s4 like-section">
+                        {this.renderLikeButton()}
                       </div>
                     </div>
                   </div>
 
-                  {/* Set an alert when rating */}
-                  {
-                    this.state.trackRated ?
-                    <div class="alert success add-top-space">
-                      <p>Thanks for rating this track!</p>
+                  <iframe
+                    width="100%"
+                    height="300"
+                    scrolling="no"
+                    style={{ border: "0" }}
+                    allow="autoplay"
+                    src={trackUrl}
+                  />
+
+                  <div className="track-bottom">
+                    <div className="row profile-entry track-authors">
+                      <Link to={"/track/" + track._id}>Learn more</Link>
                     </div>
-                    : <span />
-                  }
+                    <div className="row rating-section">
+                      {this.renderRating(track.currentRating)}
+                    </div>
+                  </div>
                 </div>
+
+                {/* Set an alert when rating */}
+                {this.state.trackRated ? (
+                  <div class="alert success add-top-space">
+                    <p>Thanks for rating this track!</p>
+                  </div>
+                ) : (
+                  <span />
+                )}
               </div>
             </div>
+          </div>
         );
     }
   }
