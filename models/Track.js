4176492c9aa78e6e6e8fc8b7ae0ce66dc82eff17
alexjackhughes <hughes@huckletree.com>
2018-09-03 17:58:55 +0100

    Added some more default values

--- models/Track.js
+++ models/Track.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 /* Create Schema */
 const trackSchema = new Schema({
   id: String,
-  title: String,
+  title: { type: String, default: "Song Track" },
   soundCloudUrl: String,
-  type: String,
-  blurb: String,
+  type: { type: String, default: "progress" },
+  blurb: { type: String, default: "Can you sing with all the voices of the mountains, can you paint with all the colors of the wind?" },
   artists: [String],
-  ratings: [{
-              "id":String,
-              "rating":Number
-            }],
+  ratings: [
+    {
+      id: String,
+      rating: Number
+    }
+  ],
   currentRating: Number,
   genres: [String],
   instruments: [String]
 });
 
 /* Assign Schema to Collection */
-mongoose.model('tracks', trackSchema);
+mongoose.model("tracks", trackSchema);
