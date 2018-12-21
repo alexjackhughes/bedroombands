bc138aa048cb5edf76a1f669f06d144e01dbc814
alexjackhughes <hughes@huckletree.com>
2018-12-21 12:26:45 +0000

    Added a random username generator

--- models/User.js
+++ models/User.js
@@ -2,13 +2,19 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 /* Create Schema */
+let x = ['Purple', 'Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Orange', 'Burgundy', 'Byzantium', 'Crimson', 'Coral', 'Lime', 'Ivory', 'Jade', 'Lemon', 'Lavender', 'Pear', 'Tan', 'White'];
+let y = ['Fancy', 'Plump', 'Clever', 'Shy', 'Gifted', 'Lively', 'Sparkly', 'Grumpy', 'Immense', 'Short', 'Stumpy', 'Tinkly', 'Sparkly', 'Squeaking'];
+let z = ['Monkey', 'Dinosaur', 'Cat', 'Dog', 'Artist', 'Ship', 'Mouse', 'Jellyfish', 'Ant', 'Bug', 'Spider', 'Fly', 'Clever'];
+
+let username = `${x[Math.floor(Math.random() * x.length)]} ${y[Math.floor(Math.random() * y.length)]} ${z[Math.floor(Math.random() * z.length)]}`;
+
 const userSchema = new Schema({
   googleID: String,
   credits: {
     type: Number,
     default: 0
   },
-  username: { type: String, default: String(Math.random() * 1000 + 1) },
+  username: { type: String, default: username },
   blurb: {
     type: String,
     default: "My favourite band is Jedward and maybe I should change my bio"
