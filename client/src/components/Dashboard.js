7e2a090ce3f3d0cdd3c422bf2c1a6e7dc167d26d
alexjackhughes <info@alexjackhughes.com>
2019-04-14 18:36:16 +0100

    Added Intercom

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,9 +7,18 @@ import { reduxForm, Field } from "redux-form";
 import Sidebar from "./Sidebar";
 import SurveyList from "./surveys/SurveyList";
 import TrackList from "./TrackList";
+import Intercom from "react-intercom";
 
 class Dashboard extends Component {
   render() {
+    const user = this.props.auth
+      ? {
+          user_id: this.props.auth._id,
+          email: this.props.auth.email,
+          name: this.props.auth.username
+        }
+      : null;
+
     switch (this.props.auth) {
       case null:
 
@@ -25,11 +34,12 @@ class Dashboard extends Component {
 
             <div className="col s8 main-track-section dashboard-mobile">
               <div className="row add-margin-mobile">
-                <h1 className="page-title" style={{ "textAlign": "center" }}>
+                <h1 className="page-title" style={{ textAlign: "center" }}>
                   All Tracks
                 </h1>
                 <TrackList />
               </div>
+              <Intercom appID="pphqr028" {...user} />
               <Link to="/upload/track">
                 <i className="fas fa-plus-circle upload-track" />
               </Link>
