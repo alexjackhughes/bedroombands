73282c22930124c0b249842cdbcd3ab5770225c7
Alex Lloyd <lloyd.da89@gmail.com>
2019-08-01 09:50:20 -0400

    infinite scroll w/loading component

--- client/src/components/TrackList/index.js
+++ client/src/components/TrackList/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 import Track from "../Track";
+import Loader from "../Loader";
 
 class TrackList extends React.Component {
   constructor(props) {
@@ -11,9 +12,8 @@ class TrackList extends React.Component {
   componentDidMount() {
     if (this.props.tracks === undefined) {
       let limit = this.state && this.state.limit ? this.state.limit : 10;
-
       axios.get(`/api/tracks/${limit}`).then(tracks => {
-        this.setState({ tracks: tracks.data, limit });
+        this.setState({ tracks: tracks.data, limit, hasMore: true });
       });
     } else {
       // this.setState({ tracks: null });
@@ -73,7 +73,7 @@ class TrackList extends React.Component {
     }
   }
 
-  onChangeLimit() {
+  onChangeLimit = () => {
     this.setState(
       {
         limit: this.state.limit + 10
@@ -83,11 +83,15 @@ class TrackList extends React.Component {
 
         axios.get(`/api/tracks/${this.state.limit}`).then(tracks => {
           //console.log("hello", tracks.data);
-          this.setState({ tracks: tracks.data });
+          let hasMore = true;
+          if (this.state.tracks.length == tracks.data.length) {
+            hasMore = false;
+          }
+          this.setState({ tracks: tracks.data, hasMore });
         });
       }
     );
-  }
+  };
 
   render() {
     console.log("wee", this.props.title);
@@ -98,21 +102,29 @@ class TrackList extends React.Component {
 
         default:
           return (
-            <div style={{ textAlign: "center" }}>
-              {this.state.tracks.map(track => {
-                //console.log(this.state.tracks, "tracks");
-                // here tracks should be real
-                return <Track key={track._id} track={track} />;
-              })}
-              {
-                // <button
-                //   className="blue lighten-2 waves-effect waves-light btn-large"
-                //   onClick={() => this.onChangeLimit()}
-                // >
-                //   Load Tracks
-                // </button>
-              }
-            </div>
+            <InfiniteScroll
+              style={{ overflowX: "hidden" }}
+              dataLength={this.state.tracks.length}
+              next={this.onChangeLimit}
+              hasMore={this.state.hasMore}
+              loader={<Loader />}
+            >
+              <div style={{ textAlign: "center" }}>
+                {this.state.tracks.map(track => {
+                  console.log(this.state.tracks, "tracks");
+                  // here tracks should be real
+                  return <Track key={track._id} track={track} />;
+                })}
+                {
+                  // <button
+                  //   className="blue lighten-2 waves-effect waves-light btn-large"
+                  //   onClick={() => this.onChangeLimit()}
+                  // >
+                  //   Load Tracks
+                  // </button>
+                }
+              </div>
+            </InfiniteScroll>
           );
       }
     }
